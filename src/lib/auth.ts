f30f0a00b46cbc72e73e91f8dfc6aac5c0798b2f
Gabriel H. <gabrielhono.o@gmail.com>
2026-04-12 14:44:00 +0200

    création de auth.ts dans src/lib

---
+++ src/lib/auth.ts
@@ -0,0 +1,30 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@/src/lib/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 días
+  },
+  secret: process.env.AUTH_SECRET,
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "USER",
+        input: false, // el usuario no puede establecer su propio rol
+      },
+      pronoms: {
+        type: "string",
+        required: false,
+      },
+    },
+  },
+});
