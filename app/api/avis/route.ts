9af24353490aa21caa6b46c30dc9d8a14b6d16c2
Gabriel H. <gabrielhono.o@gmail.com>
2026-04-28 11:19:17 +0200

    Récuperation id piscine / vérification piscine existente / vérificarion avis non dupliqué

--- app/api/avis/route.ts
+++ app/api/avis/route.ts
@@ -1,6 +1,6 @@
 //app/api/avis/route.ts
 
-import { error } from "console";
+import { prisma } from "@/src/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 /* Generar ruta POST para un avis (puede existir sólo si un usuario esta autentificado) */
@@ -16,11 +16,23 @@ export async function POST(req: NextRequest) {
             }, { status: 401 })
         };
 
+        /* Recuperar el id de la piscina a través del body de la request */
+        const body = await req.json()
+        const { piscineId } = body;
+
         /* Verificar que al menos uno de los criterios se completa */
 
+
         /* Verificar que la piscina existe */
+        const piscine = await prisma.piscine.findUnique({ where: { id: piscineId } })
+        if(!piscine) {
+            return NextResponse.json({ error: "Piscine non trouvée ou inexistante"}, { status: 404 })
+        }
 
         /* Verificar que el usuario no ha dejado ya un avis de esta piscina */
+        const existingAvis = await prisma.avis.findUnique({ where: {
+            userId_piscineId: { userId, piscineId }
+        }})
 
         /* Crear un avis que vincule el userId con piscineId */
 
