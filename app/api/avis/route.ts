fcc663970c840f15e1d120e5a727c698c4c668f5
Gabriel H. <gabrielhono.o@gmail.com>
2026-04-28 12:35:48 +0200

    vérification des notes

--- app/api/avis/route.ts
+++ app/api/avis/route.ts
@@ -18,14 +18,19 @@ export async function POST(req: NextRequest) {
 
         /* Recuperar el id de la piscina a través del body de la request */
         const body = await req.json()
-        const { piscineId } = body;
-
-        /* Verificar que al menos uno de los criterios se completa */
-
+        const { piscineId, 
+            noteAccessibilite, 
+            comAccessibilite, 
+            noteAccueil, 
+            comAccueil, 
+            noteBassin, 
+            comBassin, 
+            noteVestiaire, 
+            comVestiaire } = body;
 
         /* Verificar que la piscina existe */
         const piscine = await prisma.piscine.findUnique({ where: { id: piscineId } })
-        if(!piscine) {
+        if(!piscine || typeof piscineId !== "number") {
             return NextResponse.json({ error: "Piscine non trouvée ou inexistante"}, { status: 404 })
         }
 
@@ -34,9 +39,51 @@ export async function POST(req: NextRequest) {
             userId_piscineId: { userId, piscineId }
         }})
 
+        if (existingAvis) {
+            return NextResponse.json({ error: "Cet utilisateur a déjà laissé un avis sur cette piscine" }, { status: 409 })
+        }
+
+        /* Verificar que al menos uno de los criterios se completa */
+        const oneChamp =
+        noteAccessibilite !== undefined ||
+        comAccessibilite !== undefined ||
+        noteAccueil !== undefined ||
+        comAccueil !== undefined ||
+        noteBassin !== undefined ||
+        comBassin !== undefined ||
+        noteVestiaire !== undefined ||
+        comVestiaire !== undefined
+
+        if(!oneChamp) {
+            return NextResponse.json({ error: "Il faut remplir au moins un champ d'évaluation" }, { status : 400 })
+        }
+
+        /* Verificar que las notas estén entre 0 y 5 */
+        const notes = [noteAccessibilite, noteAccueil, noteBassin, noteVestiaire]
+        .filter(n => n !== undefined)
+
+        const notesValides = notes.every(n => typeof n === 'number' && n >= 0 && n <= 5);
+
+        if(!notesValides) {
+            return NextResponse.json({ error: "Les notes doivent être comprises entre 0 et 5"}, { status: 400 })
+        };
+
+
         /* Crear un avis que vincule el userId con piscineId */
+        const avis = await prisma.avis.create({
+            data: { userId,
+                piscineId,
+                note_accessibilite : noteAccessibilite, 
+                commentaire_accessibilite : comAccessibilite, 
+                note_accueil : noteAccueil, 
+                commentaire_accueil : comAccueil, 
+                note_bassin : noteBassin, 
+                commentaire_bassin : comBassin, 
+                note_vestiaires : noteVestiaire, 
+                commentaire_vestiaires : comVestiaire}
+        })
 
-
+        return NextResponse.json({ data: avis }, { status: 201 })
 
     } catch(error) {
         console.error('Erreur POST api/avis:', error)
