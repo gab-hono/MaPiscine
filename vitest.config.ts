0e208f48a1e5e8b8872c332ed2af81c268745670
Gabriel H. <gabrielhono.o@gmail.com>
2026-05-16 15:27:41 +0200

    ajout de la configuration TypeScript pour Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-    test: {
-        environment: "node",
-    },
+  test: {
+    environment: 'node',
+    cache: false,
+    exclude: ['**/node_modules/**', '**/.next/**'],
+  },
+  resolve: {
+    conditions: ['node'],
+  },
+  esbuild: {
+    target: 'node18',
+  },
 })
